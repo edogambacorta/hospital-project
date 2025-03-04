6e445041fee3dd691f3d0fe87205fd908258bf11
“Edoardo <edoardo.gambacorta@gmail.com>
2025-03-04 13:13:33 +0100

    -

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "src/styles/_variables.scss" as *;`,
+        includePaths: [path.resolve(__dirname, 'src/styles')]
+      }
+    }
+  }
 })
